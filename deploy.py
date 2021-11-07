89283adb3330060ef63a5950bc3b124e628fb5a9
Tony Tong <tony.tong@punchh.com>
2021-11-06 22:25:28 -0700

    Add deployment

--- deploy.py
+++ deploy.py
@@ -1,6 +1,10 @@
+from dotenv import load_dotenv
 import json
 import os
 from solcx import compile_standard
+from web3 import Web3
+
+load_dotenv()
 
 CWD = os.path.dirname(__file__)
 
@@ -24,3 +28,39 @@ compiled_sol = compile_standard(
 print(compiled_sol)
 with open(os.path.join(CWD, "compiled_code.json"), "w") as file:
     json.dump(compiled_sol, file)
+
+# obtain the bytecode
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+
+# get abi
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
+chain_id = 1337
+my_address = "0x669135d482892ef79b4DA5c49A37d12458697Cea"
+private_key = os.getenv("PRIVATE_KEY")
+
+
+# Create the contract in Python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+
+# Get the latest transaction
+nonce = w3.eth.getTransactionCount(my_address)
+print(f"{nonce = }\n")
+
+# Now, we are going to
+# 1. Build a transaction
+# 2. Sign a transaction
+# 3. Send a transaction
+transaction = SimpleStorage.constructor().buildTransaction(
+    {"chainId": chain_id, "from": my_address, "nonce": nonce}
+)
+
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+print(signed_txn)
+
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print(tx_receipt)
