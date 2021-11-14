bb6b72ad17afd7accfb3701ed3bcb21e4111647f
Tony Tong <tao.tong@auditoria.ai>
2021-11-14 12:44:32 -0800

    Add permutation() and reverse_permutation()

--- erc20-brownie/scripts/utils.py
+++ erc20-brownie/scripts/utils.py
@@ -1,6 +1,56 @@
 from brownie import network, config, accounts, MockV3Aggregator
+from copy import copy
+from cryptography.fernet import Fernet
+import random
+
 
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+
+def check_hashseed() -> bool:
+    if hash("GOD") == -3890164749404887474:
+        print("Hash seed confirmed.")
+        return True
+    else:
+        print("Hash seed incorrect.")
+        return False
+
+def permutate(input: str, key: str = "test"):
+    """Input should be a space delimited word string"""
+    if not check_hashseed():
+        print("Cannot continue with permutation")
+        return
+    words = [w.strip() for w in input.split()]
+    wc = len(words)
+    seq_orig = list(range(wc))
+    seq = copy(seq_orig)
+    random.seed(hash(key))
+    random.shuffle(seq)
+    return " ".join([words[i] for i in seq])
+
+def reverse_permutate(input: str, key: str = "test"):
+    """Input should be a space delimited word string"""
+    if not check_hashseed():
+        print("Cannot continue with permutation")
+        return
+    words = [w.strip() for w in input.split()]
+    wc = len(words)
+    seq_orig = list(range(wc))
+    seq = copy(seq_orig)
+    random.seed(hash(key))
+    random.shuffle(seq)
+    tp = list(zip(seq_orig, seq))
+    tp.sort(key=lambda x: x[1])
+    seq_rev = [t[0] for t in tp]
+    return " ".join([words[i] for i in seq_rev])
+
+
+def encrypt(message: bytes, key: bytes) -> bytes:
+    return Fernet(key).encrypt(message)
+
+
+def decrypt(token: bytes, key: bytes) -> bytes:
+    return Fernet(key).decrypt(token)
+
 
 
 def get_account(num: int = 0):
